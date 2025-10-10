afb432fdf2f9cc47a5b800e57f55d6cd2ea9460a
Aditya Kumar Rajput <pythonboot06@gmail.com>
2025-10-10 17:31:01 +0530

    addedd peterson solution

---
+++ PeterSon_Solution.cpp
@@ -0,0 +1,35 @@
+// while(1){
+//     while(turn!=0) 
+//     turn=1;
+// }
+
+// while(1){
+//     while(turn!=1)
+//     turn=0;
+// }
+
+// Peterson solution
+
+// flag[2] = which process is ready to enter Critical Section
+// turn = which process's turn is to enter Critical Section
+flag[i]=ture //represents that process i is ready to enter CS
+
+
+do{
+    flag[0]=true;
+    turn=1;
+    while(turn==1 && flag[1]==true); //busy wait
+    //critical section
+    flag[0]=false;
+    //remainder section
+}while(1);
+
+
+do{
+    flag[1]=true;
+    turn=0;
+    while(turn==0 && flag[0]==true); //busy wait
+    //critical section
+    flag[1]=false;
+    //remainder section
+}
